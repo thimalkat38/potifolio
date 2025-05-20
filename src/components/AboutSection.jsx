0cbdec8b23bf7298e16049141b22760019e5cb81
Thimalka Thennakoon <114043982+thimalka38kakiri@users.noreply.github.com>
2025-05-20 17:44:13 +0530

    1.0

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="/CV THIMALKA THENNAKOON.pdf"
+                href="/CV THIMALKA THENNAKOON1.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
